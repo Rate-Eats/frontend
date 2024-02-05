1731688c876135b3bd7e868d9b7c40a5c0ebc0c7
Hubert <hubert.grabke@gmail.com>
2024-02-05 12:23:55 +0100

    configure ignore files and prettier, add alliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path'
+
 
 export default defineConfig({
-  base: "/",
   plugins: [react()],
   preview: {
     port: 8080,
@@ -12,6 +13,14 @@ export default defineConfig({
     port: 8080,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:8080",
+    origin: 'http://0.0.0.0:8080',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@styles': path.resolve(__dirname, 'src/styles')
+    }
   },
 });
