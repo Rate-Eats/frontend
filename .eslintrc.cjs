17524b887f229ca6f43e08c7a6086779380b88f2
Hubert <hubert.grabke@gmail.com>
2024-02-06 10:09:04 +0100

    configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,20 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/no-rest-destructuring': 'warn',
+    '@tanstack/query/stable-query-client': 'error',
+    quotes: ['warn', 'single'],
   },
-
 };
