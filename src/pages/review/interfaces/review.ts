763d94bdc3136c0a39e059e22004bdfe8743997f
Hubert <hubert.grabke@gmail.com>
2024-03-13 19:49:29 +0100

    add review interfaces and component for no reviews with needed assets

---
+++ src/pages/review/interfaces/review.ts
@@ -0,0 +1,55 @@
+interface ReviewImageData {
+  id: number;
+  path: string;
+  name: string;
+  hash: string;
+  extension: string;
+  main: boolean;
+  menu: boolean;
+}
+
+interface ReviewUserData {
+  data: {
+    id: number;
+    attributes: {
+      username: string;
+      email: string;
+      provider: string;
+      confirmed: boolean;
+      blocked: boolean;
+      createdAt: string;
+      updatedAt: string;
+      avatar: string;
+    };
+  };
+}
+
+interface ReviewRestaurantData {
+  data: {
+    id: number;
+    attributes: {
+      name: string;
+      createdAt: string;
+      updatedAt: string;
+      publishedAt: string;
+      description: string;
+      address: string;
+    };
+  };
+}
+export interface ReviewData {
+  id: number;
+  attributes: {
+    rating_food: number;
+    rating_ambience: number;
+    rating_price: number;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    rating_service: number;
+    images: ReviewImageData[];
+    users: ReviewUserData;
+    restaurant: ReviewRestaurantData;
+  };
+}
