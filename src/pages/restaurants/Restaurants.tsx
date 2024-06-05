bd134f21bb3a45c209e96b4222d9a3c3b5966951
Hubert <hubert.grabke@gmail.com>
2024-06-05 23:25:49 +0200

    apply selected filters to get restaurants api call, remove minimum maximum rating selection from filter

--- src/pages/restaurants/Restaurants.tsx
+++ src/pages/restaurants/Restaurants.tsx
@@ -2,19 +2,28 @@ import ErrorFetching from '@components/states/errorFetching/ErrorFetching.tsx';
 import RestaurantsSkeleton from '@pages/restaurants/RestaurantsSkeleton.tsx';
 import { getRestaurants } from '@pages/restaurants/utils/getRestaurants.ts';
 import Filter from '@pages/restaurants/components/Filter.tsx';
+import { useSearchParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
 const Restaurants = () => {
+  const [searchParams] = useSearchParams();
+  const params = {
+    search_query: searchParams.get('search_query'),
+    categories: searchParams.getAll('category'),
+    minimum_rating: searchParams.get('minimum_rating'),
+    maximum_rating: searchParams.get('maximum_rating'),
+  };
+
   const { error, data, isFetching } = useQuery({
-    queryKey: ['restaurants'],
-    queryFn: () => getRestaurants(),
+    queryKey: ['restaurants', params],
+    queryFn: () => getRestaurants(params),
     refetchOnWindowFocus: false,
   });
   if (isFetching) return <RestaurantsSkeleton />;
   if (error || !data) return <ErrorFetching errorText={'There was an error loading this restaurant'} />;
 
   return (
-    <div className="max-w-screen-xl mx-auto gap-5 py-6">
+    <div className="mx-auto max-w-screen-xl gap-5 py-6">
       <Filter />
     </div>
   );
--- src/pages/restaurants/utils/getRestaurants.ts
+++ src/pages/restaurants/utils/getRestaurants.ts
@@ -3,14 +3,23 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const getRestaurants = async (): Promise<RestaurantData> => {
-  const { data } = await axios.get(
-    `${API_URL}/restaurants`,
-    {
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_GET_RESTAURANT_TOKEN}`,
-      },
+interface ParamsInterface {
+  search_query: string | null;
+  categories: string[];
+  minimum_rating: string | null;
+  maximum_rating: string | null;
+}
+export const getRestaurants = async (params: ParamsInterface): Promise<RestaurantData> => {
+  const { search_query, categories, minimum_rating, maximum_rating } = params;
+
+  const { data } = await axios.get(`${API_URL}/restaurants?populate=categories`, {
+    params: {
+      'filters[categories][value][$in]': categories,
+      'filters[name][$contains]': search_query,
     },
-  );
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_GET_RESTAURANT_TOKEN}`,
+    },
+  });
   return data.data || null;
 };
