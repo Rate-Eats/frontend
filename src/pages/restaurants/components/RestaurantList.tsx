f978ea0bca853beabc1e00ec265e64f9256c66fa
Hubert <hubert.grabke@gmail.com>
2024-06-12 23:26:32 +0200

    add keys to map elements, adjust names, and destructure restaurant data

--- src/pages/restaurants/components/RestaurantList.tsx
+++ src/pages/restaurants/components/RestaurantList.tsx
@@ -7,44 +7,44 @@ import { useNavigate } from 'react-router-dom';
 import { Badge } from '@shared/ui/badge.tsx';
 import React from 'react';
 
-interface RestaurantList {
+interface RestaurantListProps {
   data: RestaurantData[] | undefined;
   isFetching: boolean;
 }
 
-const RestaurantList = ({ data, isFetching }: RestaurantList) => {
+const RestaurantList = ({ data, isFetching }: RestaurantListProps) => {
   const navigate = useNavigate();
 
   if (isFetching) return <RestaurantsSkeleton />;
-  if (!data) return <ErrorFetching errorText={'There was an error loading this restaurant'} />;
+  if (!data) return <ErrorFetching errorText="There was an error loading this restaurant" />;
 
-  const redirectToRestaurant = (id: number) => {
-    navigate(`/restaurant/${id}`);
-  };
+  const redirectToRestaurant = (id: number) => navigate(`/restaurant/${id}`);
 
   return (
     <div className="mx-auto flex w-full max-w-screen-xl flex-col gap-8 py-4">
       {data.map((restaurant, index) => {
-        const { name, description, address, images, median_rating, categories } = restaurant.attributes;
+        const { id, attributes } = restaurant;
+        const { name, description, address, images, median_rating, categories } = attributes;
+
         return (
-          <div className="flex gap-5 bg-white p-14">
-            <div className={`flex-1 ${index % 2 === 1 && 'order-last'}`} key={index}>
+          <div className="flex gap-5 bg-white p-14" key={id}>
+            <div className={`flex-1 ${index % 2 === 1 && 'order-last'}`}>
               <ImageSlider images={images} />
             </div>
             <div className={`flex flex-1 flex-col ${index % 2 === 1 && 'items-end'} mt-8 gap-3`}>
-              <span className="cursor-pointer text-2xl" onClick={() => redirectToRestaurant(restaurant.id)}>
+              <span className="cursor-pointer text-2xl" onClick={() => redirectToRestaurant(id)}>
                 {name}
               </span>
               <Stars rating={median_rating} />
               <span className="cursor-pointer underline">{address}</span>
               <div className="flex gap-2">
                 {categories.data.map((category) => (
-                  <Badge variant="secondary" className="h-[24px] cursor-pointer px-3">
+                  <Badge variant="secondary" className="h-[24px] cursor-pointer px-3" key={category.id}>
                     {category.attributes.name}
                   </Badge>
                 ))}
               </div>
-              <span className="false mt-4 line-clamp-4 max-h-24 transition-all">{description}</span>
+              <span className="mt-4 line-clamp-4 max-h-24 transition-all">{description}</span>
             </div>
           </div>
         );
