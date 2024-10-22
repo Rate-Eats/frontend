3c2de1940a9e4468d71ebf70b6f5a90cee8ec944
Hubert <hubert.grabke@gmail.com>
2024-10-22 23:47:19 +0200

    add skeleton view of homepage section 1 while in fetching state

--- src/pages/homepage/components/HomepageHeaderSkeleton.tsx
+++ src/pages/homepage/components/HomepageHeaderSkeleton.tsx
@@ -1,3 +1,28 @@
+import { Skeleton } from '@shared/ui/skeleton.tsx';
+
 export const HomepageHeaderSkeleton = () => {
-  return <div></div>;
+  return (
+    <div className="flex w-full justify-center overflow-hidden bg-white py-20">
+      <div className="mx-8 flex w-full max-w-7xl gap-8">
+        <div className="flex flex-1 flex-col justify-center">
+          <Skeleton className="mb-3 h-10 w-[90%]" />
+          <Skeleton className="mb-3 h-10 w-[70%]" />
+          <Skeleton className="h-5 w-[100%%]" />
+          <Skeleton className="mt-4 h-5 w-[100%%]" />
+          <Skeleton className="mt-4 h-5 w-[35%]" />
+        </div>
+        <div className="flex flex-[2] gap-8 max-xl:mr-[-100px]">
+          <div className="max-h-[440px] min-h-[440px] min-w-[170px] max-w-[170px] rounded-2xl max-xl:min-w-[150px] max-xl:max-w-[150px]">
+            <Skeleton className="h-full w-full" />
+          </div>
+          <div className="max-h-[440px] min-h-[440px] min-w-[370px] max-w-[370px] rounded-2xl max-xl:min-w-[350px] max-xl:max-w-[350px]">
+            <Skeleton className="h-full w-full" />
+          </div>
+          <div className="max-h-[440px] min-h-[440px] min-w-[220px] max-w-[220px] rounded-2xl max-xl:min-w-[200px] max-xl:max-w-[200px]">
+            <Skeleton className="h-full w-full" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
