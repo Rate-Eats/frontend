23a213c4c2dfc4b9fd2e858df5511102f5733311
Hubert <hubert.grabke@gmail.com>
2024-10-27 12:52:10 +0100

    reset scroll position on location change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { headerExcludedRoutes } from '@routes/headerExcludedRoutes.ts';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { ProtectedRoute } from '@/routes/protectedRoute.tsx';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { AuthProvider } from '@/auth/authProvider.tsx';
 import Navbar from '@components/navbar/Navbar.tsx';
 import PrivateRoutes from '@/routes/private.tsx';
 import 'simplebar-react/dist/simplebar.min.css';
 import PublicRoutes from '@/routes/public.tsx';
-import React, { lazy, Suspense } from 'react';
 import SimpleBar from 'simplebar-react';
 
 const Toaster = lazy(() => import('@shared/ui/sonner.tsx'));
@@ -14,6 +14,28 @@ const Toaster = lazy(() => import('@shared/ui/sonner.tsx'));
 const App = () => {
   const { pathname } = useLocation();
   const showNavbar = !headerExcludedRoutes.includes(pathname);
+
+  useEffect(() => {
+    const scrollElement = document.querySelector('.simplebar-content-wrapper');
+
+    if (scrollElement) {
+      scrollElement.scrollTop = 0;
+    }
+    const observer = new MutationObserver(() => {
+      const scrollElement = document.querySelector('.simplebar-content-wrapper');
+      if (scrollElement) {
+        scrollElement.scrollTop = 0;
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    return () => observer.disconnect();
+  }, [pathname]);
 
   return (
     <AuthProvider>
