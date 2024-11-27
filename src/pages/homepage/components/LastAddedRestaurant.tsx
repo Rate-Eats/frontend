d02450391a5b8d4672e4326e70f573ee43c27a72
Hubert <hubert.grabke@gmail.com>
2024-11-27 22:31:21 +0100

    update last added restaurant design, add populate for categories to api call

--- src/pages/homepage/components/LastAddedRestaurant.tsx
+++ src/pages/homepage/components/LastAddedRestaurant.tsx
@@ -1,7 +1,11 @@
 import LastAddedRestaurantSkeleton from '@pages/homepage/components/LastAddedRestaurantSkeleton.tsx';
 import { fetchLastAddedRestaurants } from '@pages/homepage/utils/getLastAddedRestaurant.ts';
+import ArrowRight from '@assets/svgs/icons/arrowRight.svg?react';
+import Review from '@assets/svgs/icons/review.svg?react';
+import Eye from '@assets/svgs/icons/eye.svg?react';
 import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
+import { Badge } from '@shared/ui/badge.tsx';
 
 const baseUploadsUrl = `${import.meta.env.VITE_BACKEND_URL}`;
 
@@ -39,18 +43,42 @@ const LastAddedRestaurant = () => {
             ))}
           </div>
         </div>
-        <div
-          className="flex h-full flex-1 cursor-pointer flex-col border"
-          onClick={() => redirectToRestaurant(data.documentId)}
-        >
-          <img
-            src={`${baseUploadsUrl}/${data?.images[0]?.formats?.large?.url || data.images[0].url}`}
-            alt="main image"
-            className="h-full max-h-[330px] min-h-[250px] w-full object-cover"
-          />
-          <div className="flex flex-col gap-4 p-6">
-            <span className="text-2xl">{data.name}</span>
-            <span className="max-xl:text-md line-clamp-3 text-lg text-gray-600">{data.description}</span>
+        <div className="flex-1">
+          <div className="flex h-full flex-col overflow-hidden rounded-lg border-2 border-gray-200 border-opacity-60">
+            <img
+              className="max-h-64 w-full cursor-pointer object-cover object-center"
+              src={`${baseUploadsUrl}/${data?.images[0]?.formats?.large?.url || data.images[0].url}`}
+              alt="main image"
+              onClick={() => redirectToRestaurant(data.documentId)}
+            />
+            <div className="flex flex-1 flex-col p-6 pt-4">
+              <span className="title-font mb-2 flex gap-2 text-xs font-medium tracking-widest text-gray-400">
+                {data.categories?.map((category) => <Badge variant="secondary">{category.name}</Badge>)}
+              </span>
+              <span
+                className="title-font mb-3 cursor-pointer text-lg font-medium text-gray-900"
+                onClick={() => redirectToRestaurant(data.documentId)}
+              >
+                {data.name}
+              </span>
+              <p className="mb-3 line-clamp-3 leading-relaxed text-gray-600">{data.description}</p>
+              <div className="mt-auto flex flex-wrap items-center">
+                <button
+                  className="inline-flex cursor-pointer items-center text-indigo-500"
+                  onClick={() => redirectToRestaurant(data.documentId)}
+                >
+                  Learn More
+                  <ArrowRight />
+                </button>
+                <span className="ml-auto mr-3 inline-flex items-center border-r-2 border-gray-200 py-1 pr-3 text-sm leading-none text-gray-400">
+                  <Eye />
+                  1.2K
+                </span>
+                <span className="mr-3 inline-flex items-center py-1 text-sm leading-none text-gray-400">
+                  <Review className="mr-1 mt-[-2px]" />6
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/homepage/utils/getLastAddedRestaurant.ts
+++ src/pages/homepage/utils/getLastAddedRestaurant.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const fetchLastAddedRestaurants = async (): Promise<Restaurant> => {
   const { data } = await axios.get(
-    `${import.meta.env.VITE_API_URL}/restaurants?populate=images&pagination[page]=1&pagination[pageSize]=1&sort=publishedAt:desc`,
+    `${import.meta.env.VITE_API_URL}/restaurants?populate=images&populate=categories&pagination[page]=1&pagination[pageSize]=1&sort=publishedAt:desc`,
     {
       headers: {
         Authorization: `Bearer ${import.meta.env.VITE_GET_RESTAURANT_TOKEN}`,
